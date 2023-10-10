b9c7e40a7badba5da75df8425f35057615898602
SokySergeant <sokysergent147@gmail.com>
2023-10-10 12:37:33 +0200

    Added enemy that chases player and shoots

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] private float Speed = 2f;
+    [SerializeField] private float TimeBetweenShots = 1f;
+
+    [HideInInspector] public Transform PlayerTrans;
+    [SerializeField] private GameObject Projectile;
+
+    void Start()
+    {
+        StartCoroutine(Shoot());
+    }
+
+    void Update()
+    {
+        if(PlayerTrans)
+        {
+            Vector2 Dir = PlayerTrans.position - transform.position;
+            transform.rotation = Quaternion.LookRotation(transform.forward, Dir);
+            transform.position += transform.up * Speed * Time.deltaTime;
+        }
+    }
+
+    private IEnumerator Shoot()
+    {
+        while(true)
+        {
+            Instantiate(Projectile, transform.position, transform.rotation);
+            yield return new WaitForSeconds(TimeBetweenShots);
+        }
+    }
+}
