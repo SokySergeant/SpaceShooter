253c908c8f5fcb8e3dd49fa5c8ee007a749d59ef
SokySergeant <sokysergent147@gmail.com>
2023-10-15 15:04:24 +0200

    Added health pack spawner

---
+++ Assets/Scripts/HealthpackSpawner.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthpackSpawner : MonoBehaviour
+{
+    [SerializeField] private float TimeBetweenSpawns = 1f;
+    [SerializeField] private float TimeDeviationBetweenSpawns = 0.5f;
+
+    [SerializeField] GameObject Healthpack;
+
+    void Start()
+    {
+        StartCoroutine(SpawnHealthpacks());
+    }
+
+    private IEnumerator SpawnHealthpacks()
+    {
+        while(true)
+        {
+            float RandX = Random.Range(-8.9f, 8.9f);
+            float RandY = Random.Range(-5f, 5f);
+            Instantiate(Healthpack, new Vector2(RandX, RandY), Quaternion.identity);
+
+            yield return new WaitForSeconds(TimeBetweenSpawns + Random.Range(-1f, 1f) * TimeDeviationBetweenSpawns);
+        }
+    }
+}
